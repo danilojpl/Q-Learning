020490057068b5ea6f6eb4314e3077333c44612b
danilojpl <danilo.duartejp@outlook.com>
2021-05-29 14:29:26 -0300

    escolher-main

--- src/main.py
+++ src/main.py
@@ -1,30 +1,35 @@
 from config import GRID
 from treinar import Treinar 
 from agente import estadoTerminal
-from grid import melhorCaminho 
+from grid import melhorCaminho
 
+def escolher():
+    x = input("linha: ")
+    y = input("coluna: ")
+    while estadoTerminal(int(x),int(y),GRID):
+        print ("vaga ocupada, digite novamente: ")
+        x = input("linha: ")
+        y = input("coluna: ")
+    return x, y
 
 aprender = Treinar()
 aprender.criarDicionario(GRID)
 recompensas = aprender.calcPontuacao(GRID)
 
 print ("digite a linha e coluna onde o carro está estacionado")
-x = input("linha: ")
-y = input("coluna: ")
-while estadoTerminal(int(x),int(y),GRID):
-    print ("vaga ocupada, digite novamente: ")
-    x = input("linha: ")
-    y = input("coluna: ")
+x, y = escolher()
 
 while True:
     melhorCaminho(recompensas,int(x),int(y))
-    print ("deseja testar em um novo local?:\ndigite 1 para sim ou 0 para finalizar o programa: ")
+    print ("deseja testar em um novo local?:\ndigite 1 para sim ou qualquer outra tecla para finalizar o programa: ")
     resp = input()
-    if (resp == "0"):
-        break
+    if (resp == "1"):
+        x, y = escolher()
     else:
-        x = input("linha: ")
-        y = input("coluna: ")
+        break
+        
+  
+            
 
         	
 
